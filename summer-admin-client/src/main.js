8d5e18dcbddf2cd4c070991aa015404a022c5ac1
jqjq <ji.qi@outlook.com>
2020-04-09 13:58:26 +0800

    Update main.js

--- summer-admin-client/src/main.js
+++ summer-admin-client/src/main.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-import axios from 'axios'
+import axios from 'axios';
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-axios.defaults.baseURL = 'https://localhost:8080/api'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8080/api';
+axios.defaults.withCredentials = true;
 axios.interceptors.response.use(
   response => response,
   error => {
@@ -19,10 +19,9 @@ axios.interceptors.response.use(
       store.commit('logout')
       router.replace('/login')
     }
-    // 返回接口返回的错误信息
     return Promise.reject(error)
-  })
-Vue.prototype.$axios = axios
+  });
+Vue.prototype.$axios = axios;
 
 Vue.use(ElementUI);
 
@@ -30,4 +29,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
